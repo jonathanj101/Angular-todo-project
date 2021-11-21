b7cea84ac2ecdce1ff813d339b93a0a56375e996
jonathanj101 <jonatjimen@gmail.com>
2021-11-20 21:50:02 -0500

    porject initialized

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // property that the html component can use or display data
+  name = 'Jon';
+}
