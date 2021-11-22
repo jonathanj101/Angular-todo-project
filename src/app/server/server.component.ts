02f1261861558dab879b9e8991afb4a349b4b3ff
jonathanj101 <jonatjimen@gmail.com>
2021-11-21 19:05:22 -0500

    creating a new component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+// decorator to enhance the component
+// configuring the decorator
+@Component({
+    selector: "app-server",
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css']
+})
+export class ServerComponent {
+
+}
